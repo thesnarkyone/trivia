ea8738c24d84053a97ff9cdef5597880d05751ee
Patrick <snarky_one@hotmail.com>
2015-10-20 06:45:35 -0400

    last changes 1020

--- trivia/routes.py
+++ trivia/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for;
+﻿from flask import Flask, url_for, request, render_template;
 from app import app;
 
 # Server /
@@ -18,10 +18,43 @@ def hello():
 # server/create
 @app.route('/create')
 def create():
-    return "<h2>This is the create page!</h2>";
+    if request.method == 'GET':
+        #send the user the form
+        return render_template('createquestion.html');
+    elif request.method =='POST':
+        #read for data and save it
+        title = request.form['title'];
+        question = request.form['question'];
+        answer = request.form['answer'];
+    #store this in the database
+        return render_template('createdquestion.html',question=question);
+  
+    else: 
+        return "<h2>Invalid request</h2>";
 
 
 # server/question/<title>
-@app.route('/question/<title>')
+@app.route('/question/<title>', methods=['GET', 'POST'])
 def question(title):
-    return '<h2>' + title + '</h2>';
+    if request.method == 'GET':
+        #send the user the form
+
+        question = 'Question here.';
+        #read question from the data store
+        return render_template('answerquestion.html', question = question);
+
+    elif request.method == 'POST':
+        #User has submitted Question
+        submittedanswer - request.form['submittedanswer'];
+
+        #read answer from form
+        answer = 'answer';
+
+        if submittedanswer == answer:
+            return render_template('Correct.html');
+        else:
+            return render_template('incorrect.html', submittedanswer = submittedanswer, answer = answer);
+
+    else:
+
+        return '<h2>Invalid Request</h2>';
